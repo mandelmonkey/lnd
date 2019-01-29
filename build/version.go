c8aa5b612d67ff73a2e5763495e1e534cfd8ca7d
“christian <christian@cdwsydeny.com.au>
2019-01-29 19:10:59 +0900

    updated ver

--- build/version.go
+++ build/version.go
@@ -27,7 +27,7 @@ const (
 
 	// appPreRelease MUST only contain characters from semanticAlphabet
 	// per the semantic versioning spec.
-	appPreRelease = "beta3"
+	appPreRelease = "beta4"
 )
 
 // Version returns the application version as a properly formed string per the
