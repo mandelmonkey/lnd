9518622a80ee05ae470b37be5ac8d392f69da749
christian <chrismoss411@gmail.com>
2019-05-29 22:07:11 +0900

    na

---
+++ mobile/bindings.go
@@ -0,0 +1,33 @@
+package lndmobile
+
+import (
+	"fmt"
+	"os"
+
+	flags "github.com/jessevdk/go-flags"
+	"github.com/lightningnetwork/lnd/daemon"
+)
+
+// TODO: move to build script.
+//go:generate go build gen_bindings.go
+//go:generate ./gen_bindings.sh
+//go:generate rm ./gen_bindings
+
+// Start Function
+func Start(appDir string, callback Callback) {
+	// Call the "real" main in a nested manner so the defers will properly
+	// be executed in the case of a graceful shutdown.
+	go func() {
+		if err := daemon.LndMain(appDir, lightningLis, unlockerLis, invoicesLis); err != nil {
+			if e, ok := err.(*flags.Error); ok &&
+				e.Type == flags.ErrHelp {
+			} else {
+				fmt.Fprintln(os.Stderr, err)
+			}
+			os.Exit(1)
+		}
+	}()
+
+	// TODO(halseth): callback when RPC server is running.
+	callback.OnResponse([]byte("started"))
+}
---
+++ mobile/build_mobile.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+set -e -u -o pipefail # Fail on error
+
+dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+echo "current dir is $dir"
+cd $dir
+
+arg=${1:-}
+ 
+package="github.com/lightningnetwork/lnd/mobile"
+
+mobile_dir="$dir"
+build_dir="$mobile_dir/build"
+mkdir -p $build_dir
+
+# Generate API bindings.
+#api_filename="api_generated2.go"
+#echo "Generating mobile bindings ($api_filename)."
+#go generate
+#echo "Done."
+
+#ios_dir="$build_dir/ios"
+#mkdir -p $ios_dir
+#ios_dest="$ios_dir/Lndmobile.framework"
+#echo "Building for iOS now ($ios_dest)..."
+#"$GOPATH/bin/gomobile" bind -target=ios -tags="ios" -v -o "$ios_dest" "$package"
+
+android_dir="$build_dir/android"
+mkdir -p $android_dir
+android_dest="$android_dir/lndmobile.aar"
+echo "Building for Android ($android_dest)..."
+"$GOPATH/bin/gomobile" bind -target=android -tags="android invoicesrpc" -v -o "$android_dest" "$package"
+
+echo "Cleaning up."
+#rm $api_filename
